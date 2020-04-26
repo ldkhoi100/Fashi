26264248c23bbb7709712e9ada88d6bc46317a30
ldkhoi100 <ldkhoi100@gmail.com>
2020-04-26 15:31:51 +0700

    fashi

--- resources/views/fashi/footer.blade.php
+++ resources/views/fashi/footer.blade.php
@@ -311,7 +311,7 @@
             type : 'GET',
         })
         var ajax2 = $.ajax({
-            url : "{{ route('deleteListCart') }}",
+            url : "{{ route('updateDeleteListCart') }}",
             type : 'GET',
         });
         $.when(ajax1, ajax2).done(function(res1, res2){
